4c7477602cb6cef003144e0b15a368e19597c253
Petru_Hoarca <hoarcapetru@gmail.com>
2024-02-07 21:00:20 +0200

    update

---
+++ modul3/app4.py
@@ -0,0 +1,16 @@
+# parola = 7788
+# for incercare in range(99999):
+#     parola_introdusa = int(input('Introduceti parola: '))
+#     if parola == parola_introdusa :
+#         print('access granted')
+#         break
+#     else:        print('Parola gresita')
+
+parola = 7788
+while True:
+    parola_introdusa = int(input('Introduceti parola: '))
+    if parola == parola_introdusa:
+        print('access granted')
+        break
+    else:
+        print('Parola gresita')
--- modul3/app_lists.py
+++ modul3/app_lists.py
@@ -1,7 +1,7 @@
 duplicated = [1, 3, 5, 7, 7, 7, 7, 7, 9, 3]  # remove duplicates
 
 new_list = []
-for number in duplicated[::]:
+for number in duplicated[::]:    #tot timpul incercati sa copiati o lista inainte da face modificari pe lista respectiva
     if number not in new_list:
         new_list.append(number)
     else:
